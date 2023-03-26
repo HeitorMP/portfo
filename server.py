5086437d9a07d9be560068e45ee8a14a33142a54
heitormp <heitormp@protonmail.com>
2023-03-26 21:42:26 +0100

    portfo

---
+++ server.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+
+app = Flask(__name__)
+
+database = open('database.txt', 'a')
+
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+def write_to_csv(data):
+    with open('database.csv', mode='a', newline='') as database:
+        name = data['name']
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(
+            database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([name, email, subject, message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    try:
+        if request.method == 'POST':
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('thankyou.html')
+    except:
+        return 'did not save to database'
+    else:
+        return 'something went wrong'
